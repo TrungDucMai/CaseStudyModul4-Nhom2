7ff77dfe9c8ae886973ee707ff9f3c33a876bcf3
tranngocdai <85327489+tranngocdai@users.noreply.github.com>
2021-10-08 17:16:55 +0700

    dai

--- src/main/java/com/example/casestudymodul4nhom2/controller/RestUserController.java
+++ src/main/java/com/example/casestudymodul4nhom2/controller/RestUserController.java
@@ -1,12 +1,40 @@
 package com.example.casestudymodul4nhom2.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.casestudymodul4nhom2.model.Entity.Product;
+import com.example.casestudymodul4nhom2.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 
 @RestController
 @RequestMapping("/user")
 @CrossOrigin(origins = "*")
 public class RestUserController {
+    @Autowired
+    HttpSession httpSession;
+
+    @Autowired
+    IProductService productService;
+    @GetMapping("/showProduct")
+    public ResponseEntity<Iterable<Product>> listProduct(){
+        Iterable<Product> listProduct= productService.findAll();
+        return new ResponseEntity<>(listProduct, HttpStatus.OK);
+    }
+
+    @GetMapping("/findAllByName/")
+    public ResponseEntity<Iterable<Product>> listByNameProduct(@RequestParam("name") String name){
+        Iterable<Product> listByNameProduct= productService.findAllByNameContaining(name);
+        return new ResponseEntity<>(listByNameProduct,HttpStatus.OK);
+    }
+
+    @GetMapping("/findAllByPrice/")
+    public ResponseEntity<Iterable<Product>> listByPriceProduct(@Param("lowestPrice") double lowestPrice, @Param("highestPrice") double highestPrice){
+        Iterable<Product> listProductByPrice= productService.findAllByPriceBetween(lowestPrice,highestPrice);
+        return new ResponseEntity<>(listProductByPrice,HttpStatus.OK);
+    }
 }
