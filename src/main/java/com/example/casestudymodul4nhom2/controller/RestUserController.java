5fe08f78bd4abd2a8930dc775b3d013d67a38f25
ngnganh2207 <80036043+ngnganh2207@users.noreply.github.com>
2021-10-08 17:10:52 +0700

    update 8.10.21

--- src/main/java/com/example/casestudymodul4nhom2/controller/RestUserController.java
+++ src/main/java/com/example/casestudymodul4nhom2/controller/RestUserController.java
@@ -1,7 +1,10 @@
 package com.example.casestudymodul4nhom2.controller;
 
 import com.example.casestudymodul4nhom2.model.Entity.Cart;
+import com.example.casestudymodul4nhom2.model.Entity.Product;
+import com.example.casestudymodul4nhom2.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +16,31 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+//Kết nối vs fontend
 @CrossOrigin(origins = "*")
 public class RestUserController {
     @Autowired
     HttpSession httpSession;
+
+    @Autowired
+    IProductService productService;
+    @GetMapping("/showProduct")
+    public ResponseEntity<Iterable<Product>> listProduct(){
+        Iterable<Product> listProduct= productService.findAll();
+        return new ResponseEntity<>(listProduct,HttpStatus.OK);
+    }
+
+    @GetMapping("/findAllByName/")
+    public ResponseEntity<Iterable<Product>> listByNameProduct(@RequestParam("name") String name){
+        Iterable<Product> listByNameProduct= productService.findAllByNameContaining(name);
+        return new ResponseEntity<>(listByNameProduct,HttpStatus.OK);
+    }
+
+    @GetMapping("/findAllByPrice/")
+    public ResponseEntity<Iterable<Product>> listByPriceProduct(@Param("lowestPrice") double lowestPrice, @Param("highestPrice") double highestPrice){
+        Iterable<Product> listProductByPrice= productService.findAllByPriceBetween(lowestPrice,highestPrice);
+        return new ResponseEntity<>(listProductByPrice,HttpStatus.OK);
+    }
 
   //  @Autowired
 
--- src/main/java/com/example/casestudymodul4nhom2/service/IProductService.java
+++ src/main/java/com/example/casestudymodul4nhom2/service/IProductService.java
@@ -6,4 +6,6 @@ import com.example.casestudymodul4nhom2.model.User.AppUser;
 public interface IProductService extends IGeneralService<Product> {
     Iterable<Product> findProductByUser(AppUser user);
 
+    Iterable<Product> findAllByNameContaining(String name);
+    Iterable<Product> findAllByPriceBetween(double lowestPrice, double highestPrice);
 }
